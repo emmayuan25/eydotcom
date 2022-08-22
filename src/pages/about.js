540ddbab8aafd23f674c1a3bb8027476f9ed6811
Emma Yuan <jinyueyu@usc.edu>
2022-08-21 18:31:55 -0700

    update about

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,7 +11,11 @@ export default function about() {
     <div className='about-page'>
         <Header />
         <div className='about-container'>
-          <p className='intro-text'>Hi there, it's Emma =]</p>
+          <p className='intro-text mb-4'>Hi there, it's Emma =]</p>
+          <p className='mb-2'>I am a Junior at University of Southern California pursuing a Bachelor of Science in Computer Science and a Bachelor of Arts in Cognitive Science.</p>
+          <p className='mb-2'>Most of my experiences has been around full-stack web development, with more emphasis on the frontend side, but I am open to learning any kind of new technologies!</p>
+          <p className='mb-2'>For the upcoming school year, I'll be looking for an internship in software engineering for Summer 2023, and I'm thinking about learning mobile development and dive deeper into the backend side. Connect me via LinkedIn or Email!</p>
+          <p className='mb-4'>Fun Fact: I failed the behind-the-wheel test three times and no, getting a DL is not on my wish list this year. But I do enjoy outdoor activities and exploring California, so if you have a car and enjoys hiking with awkward convos, hit me up and I'll buy you lunch in the campsite of Mt. Whitney :p</p>
 
           <div className='link-container'>
             <ButtonLink 
@@ -41,9 +45,15 @@ export default function about() {
             <Button text="C++" />
             <Button text="JavaScript" />
             <Button text="React" />
-            <Button text="HTML/CSS" />
+            <Button text="HTML/JSX" />
             <Button text="Java" />
+            <Button text="PHP" />
             <Button text="Python" />
+            <Button text="MySQL" />
+            <Button text="Node.js" />
+            <Button text="TailwindCSS" />
+            <Button text="AWS Amplify" />
+            <Button text="Heroku" />
           </div>
 
         </div>
